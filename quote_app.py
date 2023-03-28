a3c913e8350e9c223db5012910c71ad90f730268
masoudsakhi <masoud90@hotmail.com>
2023-03-28 21:19:00 +0200

    some changed.

--- quote_app.py
+++ quote_app.py
@@ -1,7 +1,7 @@
 import redis
 import json
 import requests
-
+import random
 
 redis_connection = redis.Redis(
     host='localhost',
@@ -10,21 +10,40 @@ redis_connection = redis.Redis(
     decode_responses=True
 )
 
+def store_quotes():
+    response = requests.get("https://dummyjson.com/quotes")
+    quotes = json.loads(response.text)
+    redis_connection.set('quotes', json.dumps(quotes))
 
-def rand_quote():
-    get_quote = input("If you would like a random quote, type 'y' and enter: ")
-    if get_quote == 'y':
-        print(redis_connection.randomkey())
-    else: pass
+def get_quote():
+    quotes = json.loads(redis_connection.get('quotes'))
+    quote = random.choice(quotes['quotes'])
+    return quote
 
-def add_quote():
-    add_quote = input("If you would like to add your own quote, type 'y' and enter: ")
-    if add_quote == 'y':
-        new_quote = input("Please enter your quote followed by enter: ")
-        new_author = input("Please enter your name followed by enter: ")
-        print("Thank you for your input!")
-    else: pass
-    redis_connection.set(new_quote, new_author)
+def add_quote(quote, author):
+    new_quote = {"quote": quote, "author": author}
+    quotes = json.loads(redis_connection.get('quotes'))
+    quotes['quotes'].append(new_quote)
+    redis_connection.set('quotes', json.dumps(quotes))
 
-rand_quote()
-add_quote()
+store_quotes()
+
+while True:
+    print("1. Get a random quote")
+    print("2. Add a new quote")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        quote = get_quote()
+        print(f'"{quote["quote"]}" - {quote["author"]}\n')
+    elif choice == '2':
+        quote = input("Enter the quote: ")
+        author = input("Enter the author: ")
+        add_quote(quote, author)
+        print("Quote added successfully!\n")
+    elif choice == '3':
+        print("Exiting program...")
+        break
+    else:
+        print("Invalid choice. Please try again.\n")
